008d5c8d350fd554504e1eb8f16bcfb5c4db53bc
gbeletti <gbeletti@gmail.com>
2023-03-15 19:02:25 -0300

    adds test to do the job

---
+++ worker_test.go
@@ -0,0 +1,45 @@
+package workers_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/gbeletti/workers"
+)
+
+func TestDoJob(t *testing.T) {
+	timeout := time.Millisecond * 20
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	t.Cleanup(func() {
+		cancel()
+	})
+	opt := workers.Options{
+		TotalWorkers:  3, // sets up 3 workers for the test
+		BufferChannel: 0, // channel will be unbuffered
+	}
+	workers.Start(ctx, opt)
+
+	if !workers.Alive() {
+		t.Fatal("workers should be alive")
+	}
+	var job workers.Job = func() {
+		t.Log("doing something")
+	}
+
+	// sends a job to the workers to do
+	if err := workers.DoJob(job); err != nil {
+		t.Fatal("expected to send a job for the workers, instead there is no workers running")
+	}
+
+	// wait timeout and the workers to stop
+	time.Sleep(time.Millisecond * 25)
+
+	if err := workers.DoJob(job); err == nil {
+		t.Error("all workers should have stopped, no job should have been done")
+	}
+
+	if workers.Alive() {
+		t.Error("all workers should have stopped")
+	}
+}
