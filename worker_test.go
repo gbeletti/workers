1e7c7f3e1dee884f9fec543bf7530a5400483fc2
gbeletti <gbeletti@gmail.com>
2023-03-15 19:43:38 -0300

    adds test for DoFunc

--- worker_test.go
+++ worker_test.go
@@ -43,3 +43,39 @@ func TestDoJob(t *testing.T) {
 		t.Error("all workers should have stopped")
 	}
 }
+
+func TestDoFunc(t *testing.T) {
+	timeout := time.Millisecond * 20
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	t.Cleanup(func() {
+		cancel()
+	})
+	opt := workers.Options{
+		TotalWorkers:  3, // sets up 3 workers for the test
+		BufferChannel: 0, // channel will be unbuffered
+	}
+	workers.Start(ctx, opt)
+
+	if !workers.Alive() {
+		t.Fatal("workers should be alive")
+	}
+	var job = func() {
+		t.Log("doing something")
+	}
+
+	// sends a job to the workers to do
+	if err := workers.DoFunc(job); err != nil {
+		t.Fatal("expected to send a job for the workers, instead there is no workers running")
+	}
+
+	// wait timeout and the workers to stop
+	time.Sleep(time.Millisecond * 25)
+
+	if err := workers.DoFunc(job); err == nil {
+		t.Error("all workers should have stopped, no job should have been done")
+	}
+
+	if workers.Alive() {
+		t.Error("all workers should have stopped")
+	}
+}
